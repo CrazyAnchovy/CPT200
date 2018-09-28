e0ec8288643d4e09da65200d743fdf382b370d95
Mitch Bowen <Mitchel.J.Bowen@gmail.com>
2018-09-27 22:17:32 -0500

    database fixed, code running, db populating now. about 800 so far

--- whats_in_my_fridge/scrape_testing/saved_cleaner.py
+++ whats_in_my_fridge/scrape_testing/saved_cleaner.py
@@ -1,13 +1,13 @@
-
 #bbrune5369@msn.com would like a copy of this program when 'finished' Ginger
 
 import bs4 as bs
 import urllib.request
 import sqlite3
 import time
+import csv
 
 #namespace
-url_number = 20070
+url_number = 0
 link_list = []
 string_ingredients = []
 string_directions = []
@@ -17,12 +17,11 @@ directions_string = ""
 #make the sqlite3 db comment out the following after db is created
 conn = sqlite3.connect("recipe_db.db")
 c = conn.cursor()
-c.execute("""CREATE TABLE recipe_table(
+c.execute("""CREATE TABLE IF NOT EXISTS recipe_table(
             name TEXT,
             ingredients TEXT,
             directions TEXT
         )""")
-
 
 #soup prep, getting the urls
 with open ('recipe_link_file.txt') as link_file:
@@ -42,23 +41,26 @@ class Recipe:
         self.name = name
         self.directions = directions
         self.ingredients = ingredients
-        
 
-#create database of recipes
+    #create database of recipes
 for link in link_list:
+    string_ingredients = []
+    string_directions = []
     link_source = urllib.request.urlopen(link_list[url_number]).read()
     link_soup = bs.BeautifulSoup(link_source, 'lxml')
+    link_soup.prettify()
     
     recipe_title = link_soup.find('h1')
-    print(recipe_title.text)
     recipe_title_string = recipe_title.text
+    #print(recipe_title_string)
     
     ingredient_list = link_soup.find_all('ul', class_ = 'ingredient-list')
     for item in ingredient_list:
-        print(item.text)
+        #print(item.text)
         string_ingredients.append(str(item.text))
         #make this list into one string object
     ingredient_string = ''.join(string_ingredients)
+    #print(str(ingredient_string))
     
     recipe_directions_soup = link_soup.find('div', class_ = 'directions-inner container-xs')
     for item in recipe_directions_soup.find_all('li'):
@@ -66,15 +68,19 @@ for link in link_list:
             continue
         string_directions.append(str(item.text))
         #make this list into one string object
-        print(item.get_text())
+        #print(item.get_text())
     directions_string = ''.join(string_directions)
+    #print(str(directions_string))
 
+    #csv_writer.writerow([recipe_title_string, ingredient_string, directions_string])
     #time.sleep(.5)
     url_number+=1
     print(url_number)
+    
+    
     c.execute("INSERT INTO recipe_table VALUES (?,?,?)", (recipe_title_string, ingredient_string, directions_string))
     conn.commit()
-
+    
 conn.commit()
 conn.close()
     
