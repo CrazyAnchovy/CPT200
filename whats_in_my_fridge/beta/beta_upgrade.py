886c5e2057240133ae386129fb9fb33c7f6d5cd1
Mitch Bowen <Mitchel.J.Bowen@gmail.com>
2018-11-07 19:00:23 -0600

    with the beta upgraded again after class 11/7

--- whats_in_my_fridge/beta/beta_upgrade.py
+++ whats_in_my_fridge/beta/beta_upgrade.py
@@ -9,7 +9,7 @@ ingredients_list = []
 class gui(Frame):
     def __init__(self):
         Frame.__init__(self)
-        self.master.geometry("500x400")
+        self.master.geometry("700x400")
         self.master.title('What\'s in my Fridge?')
         self.grid()
         
@@ -25,23 +25,33 @@ class gui(Frame):
         self.ingredient3_entry = Entry(self)
         self.ingredient3_entry.grid(row=2, column=1)
 
+        self.ingredient4_entry = Entry(self)
+        self.ingredient4_entry.grid(row=3, column=1)
+
+        self.ingredient5_entry = Entry(self)
+        self.ingredient5_entry.grid(row=4, column=1)
+
 
         
         self.label = Label(self, text='What Ingredient?')
         self.label.grid(row = 0, column = 0)
         
-        self.information=Listbox(self, height=20, width=20)
-        self.information.grid(row=1, column=4, columnspan=1)
+        self.information=Listbox(self, height=20, width=50)
+        self.information.grid(row=0, column=4, columnspan=1, rowspan=5)
         
     def get_recipes(self):
         ingredient_counter = 0
         ingredient1 = self.ingredient1_entry.get()
         ingredient2 = self.ingredient2_entry.get()
         ingredient3 = self.ingredient3_entry.get()
+        ingredient4 = self.ingredient3_entry.get()
+        ingredient5 = self.ingredient3_entry.get()
         
         ingredients_list.append(ingredient1)
         ingredients_list.append(ingredient2)
         ingredients_list.append(ingredient3)
+        ingredients_list.append(ingredient4)
+        ingredients_list.append(ingredient5)
         
         for ingredient in ingredients_list:
             if ingredient != '':
@@ -50,12 +60,27 @@ class gui(Frame):
         if ingredient_counter == 1:
             recipes = c.execute("SELECT DISTINCT name, ingredients, directions FROM 'recipe_table' WHERE ingredients LIKE '%{}%'".format(ingredient1))
         elif ingredient_counter == 2:
-            recipes = c.execute("SELECT DISTINCT name, ingredients, directions FROM 'recipe_table' WHERE ingredients LIKE '%{}%'".format(ingredient1))
-
+            recipes = c.execute("SELECT DISTINCT name, ingredients, directions FROM 'recipe_table' WHERE ingredients LIKE '%{}%' and ingredients LIKE '%{}%'".format(ingredient1, ingredient2))
+        elif ingredient_counter == 3:
+            recipes = c.execute("SELECT DISTINCT name, ingredients, directions FROM 'recipe_table' WHERE ingredients LIKE '%{}%' and ingredients LIKE '%{}%' and ingredients LIKE '%{}%'".format(ingredient1, ingredient2, ingredient3))
+        elif ingredient_counter == 4:
+            recipes = c.execute("SELECT DISTINCT name, ingredients, directions FROM 'recipe_table' WHERE ingredients LIKE '%{}%' and ingredients LIKE '%{}%' and ingredients LIKE '%{}%' and ingredients LIKE '%{}%'".format(ingredient1, ingredient2, ingredient3, ingredient4))
+        elif ingredient_counter == 5:
+            recipes = c.execute("SELECT DISTINCT name, ingredients, directions FROM 'recipe_table' WHERE ingredients LIKE '%{}%' and ingredients LIKE '%{}%' and ingredients LIKE '%{}%' and ingredients LIKE '%{}%' and ingredients LIKE '%{}%'".format(ingredient1, ingredient2, ingredient3, ingredient4, ingredient5))
         
         for recipe in recipes:
             self.information.insert('end', "****")
             recipe = " ".join([x for x in recipe])
             self.information.insert('end', recipe)
+        
+        
+        #the next few lines to be defined as a function for 'reset'    
+        ingredient_counter = 0
+        recipes = ''
+        for ingredient in ingredients_list:
+            ingredients_list.pop()
+        #self.information.delete(0,END)
 
+        
 gui().mainloop()
+
